f7da416a5be4b91e76a9a7657f3ac380a0b6ba0e
JacobJax <jacobjax617@gmail.com>
2023-04-28 01:54:45 +0300

    adjust call to action

--- src/components/Calltoaction.jsx
+++ src/components/Calltoaction.jsx
@@ -9,7 +9,7 @@ export default function Calltoaction() {
          '
       >
          <h1 className='text-5xl font-bold mb-6 container'>Simplify how your team works today.</h1>
-         <div className="btn-container p-2">
+         <div className="btn-container p-2 my-4">
             <a href="#" className="btn btn-white">Get Started</a>
          </div>
       </section>
