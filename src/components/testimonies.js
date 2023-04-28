1c4827ad949d57304a745f91327fb14879f9a921
JacobJax <jacobjax617@gmail.com>
2023-04-28 09:08:30 +0300

    import images

--- src/components/testimonies.js
+++ src/components/testimonies.js
@@ -1,25 +1,30 @@
+import Aliavatar from '/src/assets/avatar-ali.png'
+import Anishaavatar from '/src/assets/avatar-anisha.png'
+import Richavatar from '/src/assets/avatar-richard.png'
+import Shanaiavatar from '/src/assets/avatar-shanai.png'
+
 export default [
    {
       id: 0,
-      imageUrl: '/manage-landing-page/assets/avatar-ali.png',
+      imageUrl: Aliavatar,
       name: 'Ali Bravo',
       testimony: 'We have been able to cancel so many other subscriptions since using Manage. There is no more cros-channel confusion and everyone is much more focused.'
    },
    {
       id: 1,
-      imageUrl: '/manage-landing-page/assets/avatar-anisha.png',
+      imageUrl: Anishaavatar,
       name: 'Anisha Li',
       testimony: 'Manage has supercharged our team\'s workflow. The ability to maintain visibility on a larger milestones at all times keeps everyone motivated'
    },
    {
       id: 2,
-      imageUrl: '/manage-landing-page/assets/avatar-richard.png',
+      imageUrl: Richavatar,
       name: 'Richard Watts',
       testimony: 'Manage allows us to provide structure and process. It keeps us organized and focused. I can\'t stop recommending them to everyone I talk to'
    },
    {
       id: 3,
-      imageUrl: '/manage-landing-page/assets/avatar-shanai.png',
+      imageUrl: Shanaiavatar,
       name: 'Shanai Gouh',
       testimony: 'Their software allows us to track manage and collaborate on out projects from anywhere. It keeps the whole team in-sync without being intrusive.'
    }
