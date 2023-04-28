ae99ac3a7941b60c77ee564c5cdebc672970a790
Jacob Odero <jackjax617@gmail.com>
2023-04-28 03:05:37 +0300

    Update testimonies.js

--- src/components/testimonies.js
+++ src/components/testimonies.js
@@ -1,25 +1,25 @@
 export default [
    {
       id: 0,
-      imageUrl: '../assets/avatar-ali.png',
+      imageUrl: '../manage-landing-page/assets/avatar-ali.png',
       name: 'Ali Bravo',
       testimony: 'We have been able to cancel so many other subscriptions since using Manage. There is no more cros-channel confusion and everyone is much more focused.'
    },
    {
       id: 1,
-      imageUrl: '../assets/avatar-anisha.png',
+      imageUrl: '../manage-landing-page/assets/avatar-anisha.png',
       name: 'Anisha Li',
       testimony: 'Manage has supercharged our team\'s workflow. The ability to maintain visibility on a larger milestones at all times keeps everyone motivated'
    },
    {
       id: 2,
-      imageUrl: '../assets/avatar-richard.png',
+      imageUrl: '../manage-landing-page/assets/avatar-richard.png',
       name: 'Richard Watts',
       testimony: 'Manage allows us to provide structure and process. It keeps us organized and focused. I can\'t stop recommending them to everyone I talk to'
    },
    {
       id: 3,
-      imageUrl: '../assets/avatar-shanai.png',
+      imageUrl: '../manage-landing-page/assets/avatar-shanai.png',
       name: 'Shanai Gouh',
       testimony: 'Their software allows us to track manage and collaborate on out projects from anywhere. It keeps the whole team in-sync without being intrusive.'
    }
